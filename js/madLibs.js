6c57255d593b45c6023ab8621358a4b2b7314126
dxeware <dxeware@gmail.com>
2015-08-15 13:13:30 -0700

    Used  variables for displaying story instead of answers[] array

--- js/madLibs.js
+++ js/madLibs.js
@@ -18,33 +18,6 @@ angular.module('madLibs', [])
         $scope.verbtext = "VERB";
         $scope.hugenumtext = "HUGE NUMBER";
 
-        $scope.answers = [];
-
-        // Fill in the user's answers for story
-        $scope.fillIn = function() {
-            $scope.answers[0] = $scope.relative1;
-            $scope.answers[1] = $scope.adjective1;
-            $scope.answers[2] = $scope.adjective2;
-            $scope.answers[3] = $scope.adjective3;
-            $scope.answers[4] = $scope.person1;
-            $scope.answers[5] = $scope.adjective4;
-            $scope.answers[6] = $scope.person1;
-            $scope.answers[7] = $scope.adjective5;
-            $scope.answers[8] = $scope.verbed;
-            $scope.answers[9] = $scope.body;
-            $scope.answers[10] = $scope.verbing;
-            $scope.answers[11] = $scope.nounpl;
-            $scope.answers[12] = $scope.noun;
-            $scope.answers[13] = $scope.adverb;
-            $scope.answers[14] = $scope.verb1;
-            $scope.answers[15] = $scope.verb2;
-            $scope.answers[16] = $scope.hugenumber;
-            $scope.answers[17] = $scope.relative2;
-            $scope.answers[18] = $scope.person2;
-
-
-        };
-
         // Validity checking of answers and show story is OK
         $scope.submit = function() {
             console.log("Caught form submission!");
@@ -61,7 +34,6 @@ angular.module('madLibs', [])
             } else {
                 console.log('All inputs are OK');
                 $scope.errorMsg = "";
-                $scope.fillIn();
                 $scope.showInputs = false;
                 $scope.showStory = true;
             }
@@ -72,10 +44,6 @@ angular.module('madLibs', [])
         $scope.startOver = function() {
             $scope.showStory=false;
             $scope.showInputs=true;
-
-            for (var i = 0; i < $scope.answers.length; i++) {
-                $scope.answers[i] = "";
-            }
 
             $scope.relative1 = "";
             $scope.adjective1 = "";
